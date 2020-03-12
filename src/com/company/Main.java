98aeade2b1995b76293046ed25a5766ac8165660
yordanos <yordanos.negash.2021@student.lhschools.org>
2020-03-12 13:46:26 -0400

    Oddness method created

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
-    public static void main(String[] args)  {
-	//todo 
-    }
+ public static void main(String[] args) {
+     testOddness();
+
+ }
+ public static void testOddness(){
+     //todo eor odd
+     // todo keep number contained
+     // todo get the last digit
+     // we want to find out if the number is even or odd
+     Scanner Keybord  ;
+
+     Keybord = new Scanner( System.in);
+     System.out.println("give a posteive number ");
+
+      Keybord =new Scanner(System.in);
+      int number =Keybord.nextInt();
+
+     System.out.println("Number is odd:  " + (number % 2 != 0));
+
+
+ }
 }
